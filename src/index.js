818b6941fcb7dc7cb1de508180d0cae43cb0b742
Dan Siwiec <daniel.siwiec@gmail.com>
2021-01-28 09:35:02 -0800

    Add fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './Main'
 import * as serviceWorker from './serviceWorker'
+import './index.css'
 
 ReactDOM.render(<Main />, document.getElementById('root'))
 
