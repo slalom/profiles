c5f059d25e5e45e551425f1edf2b704374bfb582
Dan Siwiec <daniel.siwiec@gmail.com>
2021-02-02 10:15:53 -0800

    modify instructions

--- src/Instructions.js
+++ src/Instructions.js
@@ -14,7 +14,7 @@ const downloadTemplate = () => {
 const Instructions = props => (
   <div>
     <div>
-      <div style={{ textAlign: 'right' }}>Download YAML <a href="#" onClick={downloadTemplate}>template</a> | Install <a href="https://twodegrees1.sharepoint.com/teams/marketing/SitePages/Slalom-fonts.aspx" target="_blank">Mark Pro</a> fonts</div>
+      <div style={{ textAlign: 'right' }}>Download YAML <a href="#" onClick={downloadTemplate}>template</a> | Install official Slalom <a href="https://twodegrees1.sharepoint.com/teams/marketing/SitePages/Slalom-fonts.aspx" target="_blank">fonts</a></div>
     </div>
   </div>
 )
