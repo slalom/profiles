80096a75121fdc1760c51cf049755aa57b6b4a38
Dan Siwiec <daniel.siwiec@gmail.com>
2021-03-22 15:56:06 -0700

    Add analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
+
 import Main from './Main'
 import * as serviceWorker from './serviceWorker'
 import './index.css'
 
+ReactGA.initialize('UA-192828457-1')
+ReactGA.pageview(window.location.pathname + window.location.search)
 ReactDOM.render(<Main />, document.getElementById('root'))
 
 serviceWorker.register()
